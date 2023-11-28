2d8df77169a7d96153d974c3d74711ed3fa62b9e
IGOLIVIT <131347905+IGOLIVIT@users.noreply.github.com>
2023-11-28 11:57:30 +0300

    Initial Commit

---
+++ App264/App264App.swift
@@ -0,0 +1,17 @@
+//
+//  App264App.swift
+//  App264
+//
+//  Created by IGOR on 28/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct App264App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ App264/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  App264
+//
+//  Created by IGOR on 28/11/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
